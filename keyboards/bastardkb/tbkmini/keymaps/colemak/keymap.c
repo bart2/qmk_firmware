c991a5bff63877db991304fb8653a8edc46d9d6e
bart2 <bart2@users.noreply.github.com>
2021-11-14 22:50:38 -0800

    k/v swap

--- keyboards/bastardkb/tbkmini/keymaps/colemak/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/colemak/keymap.c
@@ -21,10 +21,6 @@ enum custom_keycodes {
     PARENS = SAFE_RANGE,
     CBRACE,
     BRACKETS,
-    DQUOT,
-    FREDIRECT,
-    FBLOGS,
-    BACKTICK,
     TBACKTICK,
 };
 
@@ -76,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_BSPC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_ENT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_Z, LGUI_T(KC_Z), LALT_T(KC_X), KC_C, KC_D, KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, _______,
+      _______, LGUI_T(KC_Z), LALT_T(KC_X), KC_C, KC_D, KC_K,                        KC_V,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                       LCTL_T(KC_TAB), KC_SPC, MO(2),  MO(3), OSM(MOD_LSFT), OSL(1)
                                       //`--------------------------'  `--------------------------'
